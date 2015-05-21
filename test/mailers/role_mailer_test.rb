ca5b725a3308fc8eea0799766bf44163367a1373
Julian Fischer <fischer@avarteq.de>
2015-05-21 15:47:11 +0200

    Corrects mail addresses in test

--- test/mailers/role_mailer_test.rb
+++ test/mailers/role_mailer_test.rb
@@ -9,8 +9,8 @@ class RoleMailerTest < ActionMailer::TestCase
   test "new" do
     mail = RoleMailer.new(@role)
     assert_match "Rolle", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["hr@example.org"], mail.to
+    assert_equal ["roles.service@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
