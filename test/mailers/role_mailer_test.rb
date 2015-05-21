34ab3066cf65b3cdfb17761e35ea02fe99489347
Julian Fischer <fischer@avarteq.de>
2015-05-21 12:47:14 +0200

    Adds role object to role mailer test

--- test/mailers/role_mailer_test.rb
+++ test/mailers/role_mailer_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class RoleMailerTest < ActionMailer::TestCase
+
+  setup do
+    @role = roles(:one)
+  end
+
   test "new" do
-    mail = RoleMailer.new
+    mail = RoleMailer.new(@role)
     assert_equal "New", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
