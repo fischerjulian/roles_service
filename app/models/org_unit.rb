741fecdde2c6cdf76a0cbb9f35d1b4e06ae754ac
Julian Fischer <fischer@avarteq.de>
2015-06-18 12:12:49 +0200

    Adds mutual association to org unit

--- app/models/org_unit.rb
+++ app/models/org_unit.rb
@@ -1,4 +1,8 @@
 class OrgUnit < ActiveRecord::Base
   belongs_to :parent_unit, class_name: 'OrgUnit'
+  has_many :child_units, class_name: 'OrgUnit', foreign_key: 'parent_unit_id'
+
   validates :title, presence: true
+
+  scope :roots, -> { where("parent_unit IS NULL") }
 end
