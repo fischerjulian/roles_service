81252d906807e5ab82634868b6d0f3039ba40b5c
Julian Fischer <fischer@avarteq.de>
2015-06-27 13:04:09 +0200

    Adds name to employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,10 +2,17 @@ class Employee < ActiveRecord::Base
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 
+#  alias name fullname
+
+
   has_and_belongs_to_many :roles
 
 
   def fullname
     "#{firstname} #{lastname}"
+  end
+
+  def name
+    fullname
   end
 end
